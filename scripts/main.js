3f024b239b7a2a4f36f62427c391a4dd09f817fe
Jesse Millar <hellojessemillar@gmail.com>
2015-05-27 10:28:14 -0600

    Search works

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,72 @@
+var init = function() {
+    document.getElementById('search').focus();
+};
+
+var clearResults = function() {
+    while (document.getElementById('results').firstChild) {
+        document.getElementById('results').removeChild(document.getElementById('results').firstChild);
+    }
+};
+
+var sortUnorderedList = function(ul, sortDescending) {
+    if (typeof ul == 'string')
+        ul = document.getElementById(ul);
+
+    // Idiot-proof, remove if you want
+    if (!ul) {
+        alert('The UL object is null!');
+        return;
+    }
+
+    // Get the list items and setup an array for sorting
+    var lis = ul.getElementsByTagName('LI');
+    var vals = [];
+
+    // Populate the array
+    for (var i = 0, l = lis.length; i < l; i++)
+        vals.push(lis[i].innerHTML);
+
+    // Sort it
+    vals.sort();
+
+    // Sometimes you gotta DESC
+    if (sortDescending)
+        vals.reverse();
+
+    // Change the list on the page
+    for (var i = 0, l = lis.length; i < l; i++)
+        lis[i].innerHTML = vals[i];
+};
+
 var search = function() {
-    // console.log(collectibles.cards.length);
+    if (document.getElementById('search').value.length > 0) {
+        clearResults();
 
-    var query = document.getElementById('search').value;
+        // console.log(collectibles.cards.length);
 
-    var results = [];
+        var query = document.getElementById('search').value;
 
-    for (var i = 0; i < collectibles.cards.length; i++) {
-        for (var key in collectibles.cards[i]) {
-            // console.log(key);
+        var results = [];
 
-            if (typeof collectibles.cards[i][key] == 'string') {
-                if (collectibles.cards[i][key].toLowerCase().indexOf(query.toLowerCase()) > -1) {
-                    results.push(collectibles.cards[i]);
+        for (var i = 0; i < collectibles.cards.length; i++) {
+            for (var key in collectibles.cards[i]) {
+                // console.log(key);
+
+                if (typeof collectibles.cards[i][key] == 'string') {
+                    if (collectibles.cards[i][key].toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                        results.push(collectibles.cards[i]);
+                    }
                 }
             }
         }
-    }
 
-    console.log(results);
+        for (var i = 0; i < results.length; i++) {
+            var ul = document.getElementById('results');
+            var li = document.createElement('li');
+            li.appendChild(document.createTextNode(results[i].name));
+            ul.appendChild(li);
+        }
+
+        sortUnorderedList('results');
+    }
 };
