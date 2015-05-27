427819015cbe6646b79d090fd97eb50d3472a32b
Jesse Millar <hellojessemillar@gmail.com>
2015-05-27 11:47:59 -0600

    Better search

--- scripts/main.js
+++ scripts/main.js
@@ -2,35 +2,39 @@ var init = function() {
     document.getElementById('search').focus();
 };
 
-var search = function() {
-    if (document.getElementById('search').value.length > 0) {
-        $('#results').empty(); // Clear the <ul>
+$('#form').on('submit', function(event) {
+    event.preventDefault();
 
-        var query = document.getElementById('search').value;
+    $('#results').empty(); // Clear the <ul>
 
-        var results = [];
+    var query = document.getElementById('search').value;
 
-        for (var i = 0; i < collectibles.cards.length; i++) {
-            for (var key in collectibles.cards[i]) {
-                if (typeof collectibles.cards[i][key] == 'string') {
-                    if (collectibles.cards[i][key].toLowerCase().indexOf(query.toLowerCase()) > -1) {
-                        results.push(collectibles.cards[i]);
-                    }
+    var results = [];
+
+    for (var i = 0; i < collectibles.cards.length; i++) {
+        for (var key in collectibles.cards[i]) {
+            if (typeof collectibles.cards[i][key] == 'string') {
+                if (collectibles.cards[i][key].toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                    results.push(collectibles.cards[i]);
                 }
             }
         }
+    }
 
-        $.unique(results); // Remove duplicates
+    $.unique(results); // Remove duplicates
 
+    if (results.length > 0) {
         for (var i = 0; i < results.length; i++) {
             $("#results").append('<li>' + results[i].name + '</li>');
         }
-
-        $('#results').html(
-            $('#results').children('li').sort(function(a, b) {
-                return $(a).text().toUpperCase().localeCompare(
-                    $(b).text().toUpperCase());
-            })
-        );
+    } else {
+        $("#results").append('<li>No results found</li>');
     }
-};
+
+    $('#results').html(
+        $('#results').children('li').sort(function(a, b) {
+            return $(a).text().toUpperCase().localeCompare(
+                $(b).text().toUpperCase());
+        })
+    );
+});
