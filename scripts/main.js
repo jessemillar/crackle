ade7d4e4f219d2872a56b799c9a105e2c476db74
Jesse Millar <hellojessemillar@gmail.com>
2015-05-27 16:50:52 -0600

    Card previews work

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,27 @@ var init = function() {
     document.getElementById('search').focus();
 };
 
-$('#form').on('submit', function(event) {
+$(document).mousemove(function(event) {
+    $('#card_preview').css({ // The integers are for offset so the image doesn't "cover" the cursor and cause flickering issues
+        'position': 'absolute',
+        'left': event.clientX + 12,
+        'top': event.clientY - 10
+    });
+});
+
+$('#results').on('mouseenter', 'li', function(event) {
+    var card_id = $(this).attr('card_id');
+
+    $('#card_preview').attr('src','images/cards/' + card_id + '.png');
+
+    $('#card_preview').show();
+});
+
+$('#results').on('mouseleave', 'li', function(event) {
+    $('#card_preview').hide();
+});
+
+$('#search_form').on('submit', function(event) {
     event.preventDefault();
 
     $('#results').empty(); // Clear the <ul>
@@ -25,10 +45,10 @@ $('#form').on('submit', function(event) {
 
     if (results.length > 0) { // If we have results to show
         for (var i = 0; i < results.length; i++) {
-            $("#results").append('<li>' + results[i].name + '</li>');
+            $('#results').append('<li card_id="' + results[i].id + '">' + results[i].name + '</li>');
         }
     } else {
-        $("#results").append('<li>No results found</li>');
+        $('#results').append('<li>No results found</li>');
     }
 
     $('#results').html( // Alphabetically sort the <ul>
