6cd6208b3135f7e0969ce305d64b800d704d93b1
Jesse Millar <hellojessemillar@gmail.com>
2015-05-28 09:11:18 -0600

    Fixed Sweetalert messages

--- scripts/main.js
+++ scripts/main.js
@@ -29,6 +29,8 @@ var changeMode = function(newMode) {
 };
 
 var liClick = function(id, name, description, hero, category, rarity, race, set, cost, attack, health) {
+    $('#search_results').hide();
+
     if (mode == 'browse') {
         detailAlert(id, name, description, hero, category, rarity, race, set, cost, attack, health);
     } else if (mode == 'make_deck') {
@@ -96,21 +98,21 @@ var detailAlert = function(id, name, description, hero, category, rarity, race, 
 $('.search_clear').click(function() {
     $('#search').val('');
     $('#search_results').hide();
+    $('.card_preview').hide();
 });
 
 $('#search').on('blur', function() {
-    $('#search_results').hide();
+    $('.card_preview').hide();
 });
 
 $(document).mousemove(function(event) {
     $('.card_preview').css({ // The integers are for offset so the image doesn't "cover" the cursor and cause flickering issues
-        'position': 'fixed',
         'left': event.clientX + 12,
         'top': event.clientY - 10
     });
 });
 
-$('.search_results').on('mouseenter', 'li', function(event) {
+$('#search_results').on('mouseenter', 'li', function(event) {
     var card_id = $(this).attr('card_id');
 
     if (card_id) {
@@ -120,82 +122,90 @@ $('.search_results').on('mouseenter', 'li', function(event) {
     }
 });
 
-$('.search_results').on('mouseleave', 'li', function(event) {
+$('#search_results').on('mouseleave', 'li', function(event) {
     $('.card_preview').hide();
 });
 
-$('.search_form #search').on('keyup submit', function(event) {
+$('#search_results li').click(function(event) {
     event.preventDefault();
 
-    $('#search_results').empty().show(); // Clear the <ul>
+    console.log('Clicked');
+});
 
-    var query = document.getElementById('search').value;
+$('.search_form #search').on('focus keyup submit', function(event) {
+    event.preventDefault();
 
-    var results = [];
+    if (document.getElementById('search').value.length > 0) {
+        $('#search_results').empty().show(); // Clear the <ul>
 
-    // for (var i = 0; i < collectibles.cards.length; i++) {
-    //     for (var key in collectibles.cards[i]) {
-    //         if (typeof collectibles.cards[i][key] == 'string') {
-    //             if (collectibles.cards[i][key].toLowerCase().indexOf(query.toLowerCase()) > -1) {
-    //                 results.push(collectibles.cards[i]);
-    //             }
-    //         }
-    //     }
-    // }
+        var query = document.getElementById('search').value;
 
-    for (var i = 0; i < collectibles.cards.length; i++) {
-        if (collectibles.cards[i].name.toLowerCase().indexOf(query.toLowerCase()) > -1) {
-            results.push(collectibles.cards[i]);
+        var results = [];
+
+        // for (var i = 0; i < collectibles.cards.length; i++) {
+        //     for (var key in collectibles.cards[i]) {
+        //         if (typeof collectibles.cards[i][key] == 'string') {
+        //             if (collectibles.cards[i][key].toLowerCase().indexOf(query.toLowerCase()) > -1) {
+        //                 results.push(collectibles.cards[i]);
+        //             }
+        //         }
+        //     }
+        // }
+
+        for (var i = 0; i < collectibles.cards.length; i++) {
+            if (collectibles.cards[i].name.toLowerCase().indexOf(query.toLowerCase()) > -1) {
+                results.push(collectibles.cards[i]);
+            }
         }
-    }
 
-    $.unique(results); // Remove duplicates
+        $.unique(results); // Remove duplicates
 
-    if (results.length > 0) { // If we have results to show
-        for (var i = 0; i < results.length; i++) {
-            var id, name, description, hero, category, rarity, race, set, cost, attack, health;
+        if (results.length > 0) { // If we have results to show
+            for (var i = 0; i < results.length; i++) {
+                var id, name, description, hero, category, rarity, race, set, cost, attack, health;
 
-            id = parseInt(results[i].id);
-            name = results[i].name.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
-            description = results[i].description.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
-            hero = results[i].hero;
-            hero = hero.charAt(0).toUpperCase() + hero.slice(1);
-            category = results[i].category;
-            if (category) {
-                category = category.charAt(0).toUpperCase() + category.slice(1);
-            } else {
-                category = 'None';
+                id = parseInt(results[i].id);
+                name = results[i].name.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+                description = results[i].description.replace(/[\\"']/g, '\\$&').replace(/\u0000/g, '\\0');
+                hero = results[i].hero;
+                hero = hero.charAt(0).toUpperCase() + hero.slice(1);
+                category = results[i].category;
+                if (category) {
+                    category = category.charAt(0).toUpperCase() + category.slice(1);
+                } else {
+                    category = 'None';
+                }
+                rarity = results[i].quality;
+                rarity = rarity.charAt(0).toUpperCase() + rarity.slice(1);
+                race = results[i].race;
+                race = race.charAt(0).toUpperCase() + race.slice(1);
+                set = results[i].set;
+                set = set.charAt(0).toUpperCase() + set.slice(1);
+                cost = parseInt(results[i].mana);
+
+                if (results[i].attack === null) {
+                    attack = 0;
+                } else {
+                    attack = parseInt(results[i].attack);
+                }
+
+                if (results[i].health === null) {
+                    health = 0;
+                } else {
+                    health = parseInt(results[i].health);
+                }
+
+                $('#search_results').append('<li card_id="' + id + '"><a href="#" onclick="liClick(\'' + id + '\', \'' + name + '\', \'' + description + '\', \'' + hero + '\', \'' + category + '\', \'' + rarity + '\', \'' + race + '\', \'' + set + '\', \'' + cost + '\', \'' + attack + '\', \'' + health + '\')"><b>' + results[i].name + '</b> - ' + attack + ' attack and ' + health + ' health for ' + cost + ' mana</a></li>');
             }
-            rarity = results[i].quality;
-            rarity = rarity.charAt(0).toUpperCase() + rarity.slice(1);
-            race = results[i].race;
-            race = race.charAt(0).toUpperCase() + race.slice(1);
-            set = results[i].set;
-            set = set.charAt(0).toUpperCase() + set.slice(1);
-            cost = parseInt(results[i].mana);
-
-            if (results[i].attack === null) {
-                attack = 0;
-            } else {
-                attack = parseInt(results[i].attack);
-            }
-
-            if (results[i].health === null) {
-                health = 0;
-            } else {
-                health = parseInt(results[i].health);
-            }
-
-            $('#search_results').append('<li card_id="' + id + '"onclick="liClick(\'' + id + '\', \'' + name + '\', \'' + description + '\', \'' + hero + '\', \'' + category + '\', \'' + rarity + '\', \'' + race + '\', \'' + set + '\', \'' + cost + '\', \'' + attack + '\', \'' + health + '\')"><a href="#"><b>' + results[i].name + '</b> - ' + attack + ' attack and ' + health + ' health for ' + cost + ' mana</a></li>');
+        } else {
+            $('#search_results').append('<li><a href="#">No results found</a></li>');
         }
-    } else {
-        $('#search_results').append('<li><a href="#">No results found</a></li>');
-    }
 
-    $('#search_results').html( // Alphabetically sort the <ul>
-        $('#search_results').children('li').sort(function(a, b) {
-            return $(a).text().toUpperCase().localeCompare(
-                $(b).text().toUpperCase());
-        })
-    );
+        $('#search_results').html( // Alphabetically sort the <ul>
+            $('#search_results').children('li').sort(function(a, b) {
+                return $(a).text().toUpperCase().localeCompare(
+                    $(b).text().toUpperCase());
+            })
+        );
+    }
 });
