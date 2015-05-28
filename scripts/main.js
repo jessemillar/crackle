cdd087f8ad0d80587bc4de732f86a744268aa74f
Jesse Millar <hellojessemillar@gmail.com>
2015-05-27 22:37:29 -0600

    Working alerts

--- scripts/main.js
+++ scripts/main.js
@@ -2,10 +2,10 @@ var init = function() {
     document.getElementById('search').focus();
 };
 
-var detailAlert = function(id, name, cost, attack, health) {
+var detailAlert = function(id, name, description, card_class, category, rarity, race, set, cost, attack, health) {
     swal({
         title: '<img src="images/cards/' + id + '.png" />',
-        text: 'A custom <span style="color: #F8BB86">html<span> message.',
+        text: '<table style="width:100%"><tr><td><b>Name:</b></td><td>' + name + '</td><td>Description:</td><td>' + description + '</td></tr><tr><td>Class:</td><td>' + card_class + '</td><td>Category:</td><td>' + category + '</td></tr><tr><td>Rarity:</td><td>' + rarity + '</td><td>Race:</td><td>' + race + '</td></tr><tr><td>Set:</td><td>' + set + '</td><td>Mana Cost:</td><td>' + cost + '</td></tr><tr><td>Attack:</td><td>' + attack + '</td><td>Health:</td><td>' + health + '</td></tr></table>',
         html: true
     });
 };
@@ -70,7 +70,7 @@ $('.search_form').on('submit', function(event) {
                 health = results[i].health;
             }
 
-            $('.search_results').append('<li card_id="' + results[i].id + '"onclick="detailAlert(' + results[i].id + ')"><b>' + results[i].name + '</b> - ' + attack + ' attack and ' + health + ' health for ' + results[i].mana + ' mana</li>');
+            $('.search_results').append('<li card_id="' + results[i].id + '"onclick="detailAlert(\'' + results[i].id + '\', \'' + results[i].name + '\', \'' + results[i].description + '\', \'' + results[i].hero + '\', \'' + results[i].category + '\', \'' + results[i].quality + '\', \'' + results[i].race + '\', \'' + results[i].set + '\', \'' + results[i].mana + '\', \'' + results[i].attack + '\', \'' + results[i].health + '\')"><b>' + results[i].name + '</b> - ' + attack + ' attack and ' + health + ' health for ' + results[i].mana + ' mana</li>');
         }
     } else {
         $('.search_results').append('<li>No results found</li>');
