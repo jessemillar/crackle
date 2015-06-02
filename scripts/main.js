a451799de747a431b6dab7cf699143106993a57f
jessemillar <hellojessemillar@gmail.com>
2015-06-02 16:36:06 -0600

    Fixing a single quote problem

--- scripts/main.js
+++ scripts/main.js
@@ -97,12 +97,12 @@ var populateCollection = function() {
 
     for (var i = 0; i < database.length; i++) {
         if (rowCount < 4) {
-            appendString += '<div class="col-sm-3"><img onclick="removeCard(' + database[i].id + ', \'' + database[i].name + '\')" src="images/cards/' + database[i].id + '.png" /></div>';
+            appendString += '<div class="col-sm-3"><img onclick="removeCard(' + database[i].id + ', \'' + database[i].name.replace(/'/g, '&rsquo;') + '\')" src="images/cards/' + database[i].id + '.png" /></div>';
             rowCount++;
         } else {
             $('.collection_preview').append(appendString + '</div>');
 
-            appendString = '<div class="row"><div class="col-sm-3"><img onclick="removeCard(' + database[i].id + ', \'' + database[i].name + '\')" src="images/cards/' + database[i].id + '.png" /></div>';
+            appendString = '<div class="row"><div class="col-sm-3"><img onclick="removeCard(' + database[i].id + ', \'' + database[i].name.replace(/'/g, '&rsquo;') + '\')" src="images/cards/' + database[i].id + '.png" /></div>';
 
             rowCount = 1;
         }
